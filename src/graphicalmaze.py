2fce0ecd058858a1a98f976187c28ddcd70fa7fd
Etilawin <kim.vallee@protonmail.com>
2018-12-08 13:14:13 +0100

    graphical maze is now OK with PEP

--- src/graphicalmaze.py
+++ src/graphicalmaze.py
@@ -38,96 +38,101 @@ CAN_HEIGHT = 600
 BG_COLOR = 'black'
 GRID_COLOR = 'yellow'
 
+
 def show(maze):
-	assert isinstance(maze, Maze), "The parameter must be a maze"
+    assert isinstance(maze, Maze), "The parameter must be a maze"
 
-	win = Tk()
-	win.title('Graphical maze')
-	canvas = Canvas(win, bg=BG_COLOR, width=CAN_WIDTH, height=CAN_HEIGHT)
-	canvas.pack()
+    win = Tk()
+    win.title('Graphical maze')
+    canvas = Canvas(win, bg=BG_COLOR, width=CAN_WIDTH, height=CAN_HEIGHT)
+    canvas.pack()
 
-	width = maze.get_width()
-	height = maze.get_height()
+    width = maze.get_width()
+    height = maze.get_height()
 
-	# Draw maze
-	DX = CAN_WIDTH // width
-	DY = CAN_HEIGHT // height
-	for y in range(height):
-		for x in range(width):
-			walls = maze.get_cell(y, x).get_walls()
+    # Draw maze
+    DX = CAN_WIDTH // width
+    DY = CAN_HEIGHT // height
+    for y in range(height):
+        for x in range(width):
+            walls = maze.get_cell(y, x).get_walls()
 
-			if walls[0]:
-				# Top wall
-				canvas.create_line(x * DX, y * DY,
-								   (x + 1) * DX, y * DY,
-								   fill=GRID_COLOR, width=1)
-			if walls[3]:
-				# Left wall
-				canvas.create_line(x * DX, y * DY,
-								   x * DX, (y + 1) * DY,
-								   fill=GRID_COLOR, width=1)
+            if walls[0]:
+                # Top wall
+                canvas.create_line(x * DX, y * DY,
+                                   (x + 1) * DX, y * DY,
+                                   fill=GRID_COLOR, width=1)
+            if walls[3]:
+                # Left wall
+                canvas.create_line(x * DX, y * DY,
+                                   x * DX, (y + 1) * DY,
+                                   fill=GRID_COLOR, width=1)
 
-	# Bottom line
-	canvas.create_line(0,
-					   height * DY - 1,
-					   width * DX - 1,
-					   height * DY - 1,
+    # Bottom line
+    canvas.create_line(0,
+                       height * DY - 1,
+                       width * DX - 1,
+                       height * DY - 1,
                        fill=GRID_COLOR, width=1)
-	# Right line
-	canvas.create_line(width * DX - 1,
-					   0,
-					   width * DX - 1,
-					   height * DY - 1,
+    # Right line
+    canvas.create_line(width * DX - 1,
+                       0,
+                       width * DX - 1,
+                       height * DY - 1,
                        fill=GRID_COLOR, width=1)
-	win.mainloop()
+    win.mainloop()
+
 
 def show_path(maze, path):
-	assert isinstance(maze, Maze), "The parameter must be a maze"
-	assert isinstance(path, list), "The path must be a list"
-	assert all(isinstance(cell, Cell) for cell in path), "The path must be filled with cells"
+    assert isinstance(maze, Maze), "The parameter must be a maze"
+    assert isinstance(path, list), "The path must be a list"
+    assert all(isinstance(cell, Cell) for cell in path),\
+        "The path must be filled with cells"
 
-	win = Tk()
-	win.title('Graphical path in the maze')
-	canvas = Canvas(win, bg=BG_COLOR, width=CAN_WIDTH, height=CAN_HEIGHT)
-	canvas.pack()
+    win = Tk()
+    win.title('Graphical path in the maze')
+    canvas = Canvas(win, bg=BG_COLOR, width=CAN_WIDTH, height=CAN_HEIGHT)
+    canvas.pack()
 
-	width = maze.get_width()
-	height = maze.get_height()
+    width = maze.get_width()
+    height = maze.get_height()
 
-	# Draw maze
-	DX = CAN_WIDTH // width
-	DY = CAN_HEIGHT // height
-	for y in range(height):
-		for x in range(width):
-			cell = maze.get_cell(y, x)
-			walls = cell.get_walls()
+    # Draw maze
+    DX = CAN_WIDTH // width
+    DY = CAN_HEIGHT // height
+    for y in range(height):
+        for x in range(width):
+            cell = maze.get_cell(y, x)
+            walls = cell.get_walls()
 
-			if cell in path:
-				canvas.create_oval(x * DX + DX//10, y * DY + DY//10,
-								   (x + 1) * DX - DX//10, (y + 1) * DY - DY//10,
-								   fill="red")
+            if cell in path:
+                canvas.create_oval(x * DX + DX//10,
+                                   y * DY + DY//10,
+                                   (x + 1) * DX - DX//10,
+                                   (y + 1) * DY - DY//10,
+                                   fill="red")
 
-			if walls[0]:
-				# Top wall
-				canvas.create_line(x * DX, y * DY,
-								   (x + 1) * DX, y * DY,
-								   fill=GRID_COLOR, width=1)
-			if walls[3]:
-				# Left wall
-				canvas.create_line(x * DX, y * DY,
-								   x * DX, (y + 1) * DY,
-								   fill=GRID_COLOR, width=1)
+            if walls[0]:
+                # Top wall
+                canvas.create_line(x * DX, y * DY,
+                                   (x + 1) * DX, y * DY,
+                                   fill=GRID_COLOR, width=1)
+            if walls[3]:
+                # Left wall
+                canvas.create_line(x * DX, y * DY,
+                                   x * DX, (y + 1) * DY,
+                                   fill=GRID_COLOR, width=1)
 
-	# Bottom line
-	canvas.create_line(0,
-					   height * DY - 1,
-					   width * DX - 1,
-					   height * DY - 1,
+    # Bottom line
+    canvas.create_line(0,
+                       height * DY - 1,
+                       width * DX - 1,
+                       height * DY - 1,
                        fill=GRID_COLOR, width=1)
-	# Right line
-	canvas.create_line(width * DX - 1,
-					   0,
-					   width * DX - 1,
-					   height * DY - 1,
+    # Right line
+    canvas.create_line(width * DX - 1,
+                       0,
+                       width * DX - 1,
+                       height * DY - 1,
                        fill=GRID_COLOR, width=1)
-	win.mainloop()
+    win.mainloop()
